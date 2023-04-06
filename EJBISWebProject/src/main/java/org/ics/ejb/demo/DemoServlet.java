d1a702bfc596528b2323a10b845da112fb019675
Joseph Nordin <43850387+alhethmey@users.noreply.github.com>
2023-04-06 00:30:53 +0000

    Added DemoServlet for test we can remove it later

---
+++ EJBISWebProject/src/main/java/org/ics/ejb/demo/DemoServlet.java
@@ -0,0 +1,49 @@
+package org.ics.ejb.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html><html><head>");
+		out.println("<title>ManyToManyWithattribute</title>");
+		out.println("<meta charset=\"ISO-8859-1\">");
+		out.println("</head><body>");
+		out.println("<h2>Working OK</h2>");
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
