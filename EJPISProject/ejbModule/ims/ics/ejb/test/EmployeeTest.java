678d29bf94d5599aa266766869bc34981eab7768
Alex <palexanderbengtsson@gmail.com>
2023-04-14 13:47:05 +0000

    Created first JUnit package/class

---
+++ EJPISProject/ejbModule/ims/ics/ejb/test/EmployeeTest.java
@@ -0,0 +1,81 @@
+package ims.ics.ejb.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ims.ics.ejb.Employee;
+
+class EmployeeTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testGetEmployeeId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetEmployeeId() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetAddress() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetPhoneNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetPhoneNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetPurchases() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetPurchases() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
