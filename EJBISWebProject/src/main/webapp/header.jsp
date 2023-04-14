898505a2bdddcfa9d715d90706286f19db2b50d6
EliasSirvio <94541617+EliasSirvio@users.noreply.github.com>
2023-04-14 19:05:38 +0200

    Rest Weather client (ongoing)

--- EJBISWebProject/src/main/webapp/header.jsp
+++ EJBISWebProject/src/main/webapp/header.jsp
@@ -1,8 +1,20 @@
 <header class="header">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
+</script>
+<script src="js/weather.js">
+</script>
 	<div class="container">
 		<a href="#"><img src="images/logo.png" height="80"></a>
 		<ul class="weather-box">
+					
+		
 			<li><p>Weather REST is here</p></li>
+			
+			<li><span id="degree"></span><li>
+			<li><span id="weather"></span><li>
+			<li><span></span><li>
+			<li><span></span><li>			
+			
 		</ul>
 	</div>
 </header>
---
+++ EJBISWebProject/src/main/webapp/js/weather.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+ $.ajax({
+	method: "GET",
+	url: "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&units=metric"+ "&APPID=a6c0d777f56389caaafa96fbc3ccdf87",
+	error: ajaxWeatherReturn_Error,
+	success: ajaxWeatherReturn_Success
+	})
+	
+	function ajaxWeatherReturn_Success(result, status, xhr) {
+	
+	$("#weather").text(result.weather[0].main);
+	$("#degree").text(result.main.temp+" \u2103");
+	}//ajaxWeatherReturn_Success
+	
+	function ajaxWeatherReturn_Error(result, status, xhr) {
+	alert("Error i OpenWeaterMap Ajax");
+	console.log("Ajax-find weather: "+status);
+}
