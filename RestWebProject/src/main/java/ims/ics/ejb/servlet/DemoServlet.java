9c6cca91d5654302e7458ea76213696649ce33ba
EliasSirvio <94541617+EliasSirvio@users.noreply.github.com>
2023-04-17 17:25:54 +0200

    RestAsignment

---
+++ RestWebProject/src/main/java/ims/ics/ejb/servlet/DemoServlet.java
@@ -0,0 +1,32 @@
+package ims.ics.ejb.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DemoServlet
+ */
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DemoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ RestWebProject/src/main/webapp/home.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>IMS - REST</title>
+</head>
+<body>
+<div class="main">
+<table>
+<caption>Employees</caption>
+  <thead>
+  <tr>
+    <th>Employee name</th>
+    <th>Employee Id</th>
+    <th>Address</th>
+    <th>Phone</th>
+  </tr>
+  </thead>
+  <tr>
+    <td><span id="EmployeeName"></span></td>
+    <td><span id="EmployeeId"></span></td>
+    <td><span id="EmployeeAddress"></span></td>
+    <td><span id="EmployeePhone"></span></td>
+  </tr>
+</table>
+</div>
+</body>
+</html>
