798ca2407ffd88cb65e46e6c8deff6dd3fe5fa75
AxelAlv <ax4821al-s@student.lu.se>
2023-04-20 14:54:51 +0000

    filling dropdown

--- EJPISProject/ejbModule/ims/ics/interceptors/DefaultInterceptor.java
+++ EJPISProject/ejbModule/ims/ics/interceptors/DefaultInterceptor.java
@@ -5,17 +5,17 @@ import javax.interceptor.InvocationContext;
 
 public class DefaultInterceptor {
 	
-//	@AroundInvoke
-//	public Object defaultLogger(InvocationContext iCtx) throws Exception {
-//		System.out.println("*** DefaultInterceptor: " + iCtx.getMethod().getName());
-//		System.out.println("Default - Target class: " + iCtx.getTarget().getClass());
-//		System.out.println("Default - Parameters: " + iCtx.getParameters().length);
-//		
-//		for (int i = 0; i < iCtx.getParameters().length; i++) {
-//			System.out.println("Parameter: " + iCtx.getParameters()[i].toString());
-//		}
-//		
-//		System.out.println("*********************************");
-//		return iCtx.proceed();
-//	}
+	@AroundInvoke
+	public Object defaultLogger(InvocationContext iCtx) throws Exception {
+		System.out.println("*** DefaultInterceptor: " + iCtx.getMethod().getName());
+		System.out.println("Default - Target class: " + iCtx.getTarget().getClass());
+		System.out.println("Default - Parameters: " + iCtx.getParameters().length);
+		
+		for (int i = 0; i < iCtx.getParameters().length; i++) {
+			System.out.println("Parameter: " + iCtx.getParameters()[i].toString());
+		}
+		
+		System.out.println("*********************************");
+		return iCtx.proceed();
+	}
 }
