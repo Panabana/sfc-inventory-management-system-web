cf7493f08829d8117c27a00369915f602beaed3d
EliasSirvio <94541617+EliasSirvio@users.noreply.github.com>
2023-04-24 17:20:09 +0200

    Rest Purchase

--- EJBISWebProject/src/main/java/ims/ics/ejb/servlet/RestServletPurchase.java
+++ EJBISWebProject/src/main/java/ims/ics/ejb/servlet/RestServletPurchase.java
@@ -2,9 +2,19 @@ package ims.ics.ejb.servlet;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import javax.json.JsonValue.ValueType;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +22,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ims.ics.eao.PurchaseEAOLocal;
+import ims.ics.ejb.Customer;
+import ims.ics.ejb.Employee;
 import ims.ics.ejb.Purchase;
+import ims.ics.facade.FacadeLocal;
 
 /**
  * Servlet implementation class RestServletPurchase
@@ -22,7 +35,7 @@ public class RestServletPurchase extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@EJB
-	private PurchaseEAOLocal purchaseEAO;
+	FacadeLocal facade;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -40,19 +53,21 @@ public class RestServletPurchase extends HttpServlet {
 			throws ServletException, IOException {
 
 		String pathInfo = request.getPathInfo();
-		if (pathInfo == null || pathInfo.equals("/")) {
-			List<Purchase> allPurchases = purchaseEAO.findAllPurchases();
-			sendAsJson(response, allPurchases);
-		} else {
-			String[] splits = pathInfo.split("/");
-			if (splits.length != 2) {
-				response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-				return;
-			}
-			String id = splits[1];
-			Purchase purchase = purchaseEAO.findPurchaseById(Integer.parseInt(id));
-			sendAsJson(response, purchase);
-		}
+	    if (pathInfo == null || pathInfo.equals("/")) {
+	        List<Purchase> allPurchases = facade.findAllPurchases();
+	        sendAsJson(response, allPurchases);
+	        return;
+	    }
+
+	    String[] splits = pathInfo.split("/");
+	    if (splits.length != 2) {
+	        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	        return;
+	    }
+
+	    String id = splits[1];
+	    Purchase purchase = facade.findPurchaseById(Integer.parseInt(id));
+	    sendAsJson(response, purchase);
 	}
 
 	/**
@@ -62,10 +77,30 @@ public class RestServletPurchase extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		BufferedReader reader = request.getReader();
-		Purchase purchase = parseJsonPurchase(reader);
-		purchase = purchaseEAO.createPurchase(purchase);
-		sendAsJson(response, purchase);
+		 String pathInfo = request.getPathInfo();
+
+		    // Check if the path info is valid
+		    if (pathInfo == null || pathInfo.equals("/")) {
+		        // Read the request body
+		        BufferedReader reader = request.getReader();
+		        // Parse the JSON data into a Purchase object
+		        Purchase purchase = parseJsonPurchase(reader);
+
+		        try {
+		            // Attempt to create the new purchase
+		            purchase = facade.createPurchase(purchase);
+		        } catch (Exception e) {
+		            // If the creation fails due to a duplicate key or other error, return an error response
+		            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error creating purchase.");
+		            return;
+		        }
+
+		        // Send the created purchase as a JSON response
+		        sendAsJson(response, purchase);
+		    } else {
+		        // If the path info is invalid, return a 400 Bad Request error
+		        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+		    }
 	}
 
 	/**
@@ -75,21 +110,23 @@ public class RestServletPurchase extends HttpServlet {
 			throws ServletException, IOException {
 		
 		String pathInfo = request.getPathInfo();
-        if (pathInfo == null || pathInfo.equals("/")) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-        String[] splits = pathInfo.split("/");
-        if (splits.length != 2) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-        String id = splits[1];
-        BufferedReader reader = request.getReader();
-        Purchase purchase = parseJsonPurchaseUpdate(reader);
-        purchase.setPurchaseId(Integer.parseInt(id));
-        purchaseEAO.updatePurchase(purchase);
-        sendAsJson(response, purchase);
+	    if (pathInfo == null || pathInfo.equals("/")) {
+	        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	        return;
+	    }
+
+	    String[] splits = pathInfo.split("/");
+	    if (splits.length != 2) {
+	        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	        return;
+	    }
+
+	    String id = splits[1];
+	    BufferedReader reader = request.getReader();
+	    Purchase purchase = parseJsonPurchaseUpdate(reader);
+	    purchase.setPurchaseId(Integer.parseInt(id));
+	    facade.updatePurchase(purchase);
+	    response.setStatus(HttpServletResponse.SC_NO_CONTENT);
 	}
 
 	/**
@@ -97,7 +134,146 @@ public class RestServletPurchase extends HttpServlet {
 	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String pathInfo = request.getPathInfo();
+	    if (pathInfo == null || pathInfo.equals("/")) {
+	        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	        return;
+	    }
+
+	    String[] splits = pathInfo.split("/");
+	    if (splits.length != 2) {
+	        response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+	        return;
+	    }
+
+	    String id = splits[1];
+	    Purchase purchase = facade.findPurchaseById(Integer.parseInt(id));
+	    if (purchase != null) {
+	        facade.deletePurchase(Integer.parseInt(id));
+	        sendAsJson(response, purchase);
+	    } else {
+	        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+	    }
 	}
+	
+	private void sendAsJson(HttpServletResponse response, Purchase purchase) throws IOException {
+	    response.setContentType("application/json");
+	    PrintWriter out = response.getWriter();
+	    JsonBuilderFactory factory = Json.createBuilderFactory(null);
+	    JsonObjectBuilder jsonBuilder = factory.createObjectBuilder();
+
+	    if (purchase != null) {
+	        jsonBuilder.add("purchaseId", purchase.getPurchaseId());
+	        jsonBuilder.add("employeeId", purchase.getEmployee().getEmployeeId());
+	        jsonBuilder.add("customerId", purchase.getCustomer().getCustomerId());
+	       // jsonBuilder.add("purchaseDate", purchase.getPurchaseDate().toString());
+	       // jsonBuilder.add("purchaseAmount", purchase.getPurchaseAmount());
+	    } else {
+	        // If purchase is null, add empty values to the JSON object
+	        jsonBuilder.add("purchaseId", "");
+	        jsonBuilder.add("employeeId", "");
+	        jsonBuilder.add("customerId", "");
+	        //jsonBuilder.add("purchaseDate", "");
+	       // jsonBuilder.add("purchaseAmount", "");
+	    }
+
+	    JsonObject jsonObject = jsonBuilder.build();
+	    out.print(jsonObject.toString());
+	    out.flush();
+	}
+	
+	private void sendAsJson(HttpServletResponse response, List<Purchase> purchases) throws IOException {
+	    response.setContentType("application/json");
+	    PrintWriter out = response.getWriter();
+	    JsonBuilderFactory factory = Json.createBuilderFactory(null);
+	    JsonArrayBuilder arrayBuilder = factory.createArrayBuilder();
+
+	    if (purchases != null && !purchases.isEmpty()) {
+	        for (Purchase purchase : purchases) {
+	            JsonObjectBuilder jsonBuilder = factory.createObjectBuilder();
+	            jsonBuilder.add("purchaseId", purchase.getPurchaseId());
+	            jsonBuilder.add("employeeId", purchase.getEmployee().getEmployeeId());
+	            jsonBuilder.add("customerId", purchase.getCustomer().getCustomerId());
+	            //jsonBuilder.add("purchaseDate", purchase.getPurchaseDate().toString());
+	           // jsonBuilder.add("totalAmount", purchase.getTotalAmount());
+	            JsonObject jsonObject = jsonBuilder.build();
+	            arrayBuilder.add(jsonObject);
+	        }
+	    } else {
+	        // If purchases is null or empty, add an empty array to the JSON object
+	        arrayBuilder.add(Json.createArrayBuilder().build());
+	    }
+
+	    JsonArray jsonArray = arrayBuilder.build();
+	    out.print(jsonArray.toString());
+	    out.flush();
+	}
+
+	private Purchase parseJsonPurchase(BufferedReader br) {
+	    JsonReader jsonReader = Json.createReader(br);
+	    JsonObject jsonRoot = jsonReader.readObject();
+	    
+	    Purchase purchase = new Purchase();
+	    
+	    JsonValue employeeJson = jsonRoot.get("employee");
+	    if (employeeJson != null && employeeJson.getValueType() == ValueType.OBJECT) {
+	        JsonObject employeeObj = (JsonObject) employeeJson;
+	        Employee employee = new Employee();
+	        employee.setEmployeeId(Integer.parseInt(employeeObj.getString("employeeId")));
+	        employee.setName(employeeObj.getString("employeeName"));
+	        employee.setAddress(employeeObj.getString("employeeAddress"));
+	        employee.setPhoneNumber(Integer.parseInt(employeeObj.getString("employeePhone")));
+	        purchase.setEmployee(employee);
+	    }
+	    
+	    JsonValue customerJson = jsonRoot.get("customer");
+	    if (customerJson != null && customerJson.getValueType() == ValueType.OBJECT) {
+	        JsonObject customerObj = (JsonObject) customerJson;
+	        Customer customer = new Customer();
+	        customer.setCustomerId(Integer.parseInt(customerObj.getString("customerId")));
+	        customer.setName(customerObj.getString("customerName"));
+	        customer.setAddress(customerObj.getString("customerAddress"));
+	        customer.setPhoneNbr(Integer.parseInt(customerObj.getString("customerPhone")));
+	        purchase.setCustomer(customer);
+	    }
+	    
+	    return purchase;
+	}
+
+	private Purchase parseJsonPurchaseUpdate(BufferedReader br) {
+	    JsonReader jsonReader = Json.createReader(br);
+	    JsonObject jsonRoot = jsonReader.readObject();
+	    
+	    Purchase purchase = new Purchase();
+	    purchase.setPurchaseId(Integer.parseInt(jsonRoot.getString("purchaseId")));
+	    
+	    JsonValue employeeJson = jsonRoot.get("employee");
+	    if (employeeJson != null && employeeJson.getValueType() == ValueType.OBJECT) {
+	        JsonObject employeeObj = (JsonObject) employeeJson;
+	        Employee employee = new Employee();
+	        employee.setEmployeeId(Integer.parseInt(employeeObj.getString("employeeId")));
+	        employee.setName(employeeObj.getString("employeeName"));
+	        employee.setAddress(employeeObj.getString("employeeAddress"));
+	        employee.setPhoneNumber(Integer.parseInt(employeeObj.getString("employeePhone")));
+	        purchase.setEmployee(employee);
+	    }
+	    
+	    JsonValue customerJson = jsonRoot.get("customer");
+	    if (customerJson != null && customerJson.getValueType() == ValueType.OBJECT) {
+	        JsonObject customerObj = (JsonObject) customerJson;
+	        Customer customer = new Customer();
+	        customer.setCustomerId(Integer.parseInt(customerObj.getString("customerId")));
+	        customer.setName(customerObj.getString("customerName"));
+	        customer.setAddress(customerObj.getString("customerAddress"));
+	        customer.setPhoneNbr(Integer.parseInt(customerObj.getString("customerPhone")));
+	        purchase.setCustomer(customer);
+	    }
+	    
+	
+	    
+	    return purchase;
+	}
+
+	
 
 }
