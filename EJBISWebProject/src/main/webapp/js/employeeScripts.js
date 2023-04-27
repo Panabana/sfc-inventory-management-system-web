d60958a99ba01114dae16eaf94c386a58f550371
AxelAlv <ax4821al-s@student.lu.se>
2023-04-27 11:46:19 +0000

    Removed unused method and added findById

--- EJBISWebProject/src/main/webapp/js/employeeScripts.js
+++ EJBISWebProject/src/main/webapp/js/employeeScripts.js
@@ -1,102 +1,79 @@
 
-	
-			function validateForm() {
-				var employeeName = document.getElementById("employee-name").value;
-				var employeeAddress = document.getElementById("employee-address").value;
-				var employeePhoneNumber = document.getElementById("employee-phone").value;
-				var errorMessage = "";
-	
-				 if (employeeName === "" || !/^[a-zA-ZåäöÅÄÖ]+$/.test(employeeName)) {
-					errorMessage = "Please enter a valid Name (Letters only)."
-					document.getElementById("error-label").innerHTML = errorMessage;
-					return false;
-				} else if (employeeAddress === ""
-						|| !/^[a-zA-Z0-9\såäöÅÄÖ]*$/.test(employeeAddress)) {
-					errorMessage = "Please enter an Address (Only letters and numbers allowed)."
-					document.getElementById("error-label").innerHTML = errorMessage;
-					return false;
-				} else if (employeePhoneNumber === ""
-						|| !/^\d{1,10}$/.test(employeePhoneNumber)) {
-					errorMessage = "Please enter a valid Phone Number (numbers only)."
-					document.getElementById("error-label").innerHTML = errorMessage;
-					return false;
-				} else {
-					errorMessage = "Employee was successfully added!";
-					document.getElementById("error-label").innerHTML = errorMessage;
-					return true;
-				}
+
+function validateForm() {
+	var employeeName = document.getElementById("employee-name").value;
+	var employeeAddress = document.getElementById("employee-address").value;
+	var employeePhoneNumber = document.getElementById("employee-phone").value;
+	var errorMessage = "";
+
+	if (employeeName === "" || !/^[a-zA-ZåäöÅÄÖ]+$/.test(employeeName)) {
+		errorMessage = "Please enter a valid Name (Letters only)."
+		document.getElementById("error-label").innerHTML = errorMessage;
+		return false;
+	} else if (employeeAddress === ""
+		|| !/^[a-zA-Z0-9\såäöÅÄÖ]*$/.test(employeeAddress)) {
+		errorMessage = "Please enter an Address (Only letters and numbers allowed)."
+		document.getElementById("error-label").innerHTML = errorMessage;
+		return false;
+	} else if (employeePhoneNumber === ""
+		|| !/^\d{1,10}$/.test(employeePhoneNumber)) {
+		errorMessage = "Please enter a valid Phone Number (numbers only)."
+		document.getElementById("error-label").innerHTML = errorMessage;
+		return false;
+	} else {
+		errorMessage = "Employee was successfully added!";
+		document.getElementById("error-label").innerHTML = errorMessage;
+		return true;
+	}
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	var searchForm = document.getElementById("search-form");
+	var findEmployeeBtn = document.getElementById("search-btn");
+
+	findEmployeeBtn.addEventListener("click", function(event) {
+		event.preventDefault();
+
+		var strValue = document.getElementById("find-employee-id").value;
+
+		if (strValue == "" || !/^\d{1,10}$/.test(strValue)) {
+			var errorLabel = document.getElementById("error-label");
+			errorLabel.innerText = "Please enter a valid ID to search for!";
+			return;
+		}
+
+		var tableRows = document.querySelectorAll("#employee-table tbody tr");
+		var employeeFound = false;
+
+		tableRows.forEach(function(row) {
+			var employeeId = row.cells[0].innerText;
+
+			if (employeeId === strValue) {
+				employeeFound = true;
+
+				// Retrieve customer details
+				var employeeName = row.cells[1].innerText;
+				var employeeAddress = row.cells[2].innerText;
+				var employeePhone = row.cells[3].innerText;
+
+				// Fill in the text fields
+				document.getElementById("employee-id").value = strValue;
+				document.getElementById("employee-name").value = employeeName;
+				document.getElementById("employee-address").value = employeeAddress;
+				document.getElementById("employee-phone").value = employeePhone;
 			}
-	
-			function validateSearchForm(){
-				var employeeId = document.getElementById("find-employee-id").value;
-				if(employeeId === "" || !/^\d{1,10}$/.test(employeeId)){
-					errorMessage = "Please enter a valid ID to search for!"
-						document.getElementById("error-label").innerHTML = errorMessage;
-						return false;
-				} else {
-			        var employeeExists = false;
-			        $('#employee-table tbody tr').each(function() {
-			            var rowEmployeeId = $(this).find('td:eq(0)').text();
-			            if (rowEmployeeId === employeeId) {
-			                employeeExists = true;
-			                return false; // break the loop
-			            }
-			        });
-			        if (!employeeExists) {
-			            errorMessage = "Employee with ID " + employeeId + " doesn't exist.";
-			            document.getElementById("error-label").innerHTML = errorMessage;
-			            return false;
-			        }
-			    }
-			    return true;
-			}
-			
-	document.addEventListener("DOMContentLoaded", function() {
-  var searchForm = document.getElementById("search-form");
-  var findEmployeeBtn = document.getElementById("search-btn");
+		});
 
-  findEmployeeBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    var strValue = document.getElementById("find-employee-id").value;
-
-    if (strValue == "" || !/^\d{1,10}$/.test(strValue)) {
-      var errorLabel = document.getElementById("error-label");
-      errorLabel.innerText = "Please enter a valid ID to search for!";
-      return;
-    }
-
-    var tableRows = document.querySelectorAll("#employee-table tbody tr");
-    var employeeFound = false;
-
-    tableRows.forEach(function(row) {
-      var employeeId = row.cells[0].innerText;
-
-      if (employeeId === strValue) {
-        employeeFound = true;
-
-        // Retrieve customer details
-        var employeeName = row.cells[1].innerText;
-        var employeeAddress = row.cells[2].innerText;
-        var employeePhone = row.cells[3].innerText;
-
-        // Fill in the text fields
-        document.getElementById("employee-id").value = strValue;
-        document.getElementById("employee-name").value = employeeName;
-        document.getElementById("employee-address").value = employeeAddress;
-        document.getElementById("employee-phone").value = employeePhone;
-      }
-    });
-
-    if (!employeeFound) {
-      var errorLabel = document.getElementById("error-label");
-      errorLabel.innerText = "Employee with ID: " + strValue + " not found.";
-    } else {
-      var errorLabel = document.getElementById("error-label");
-      errorLabel.innerText = "Chosen employee found.";
-    }
-  });
+		if (!employeeFound) {
+			var errorLabel = document.getElementById("error-label");
+			errorLabel.innerText = "Employee with ID: " + strValue + " not found.";
+		} else {
+			var errorLabel = document.getElementById("error-label");
+			errorLabel.innerText = "Chosen employee found.";
+		}
+	});
 });
 
 
- 
+
--- EJBISWebProject/src/main/webapp/js/purchaseScripts.js
+++ EJBISWebProject/src/main/webapp/js/purchaseScripts.js
@@ -1,17 +1,4 @@
-$(document).ready(function() {
-			$('#search-btn').click(function() {
-				$('#search-form').submit();
-			});
 
-			var purchaseId = '${purchaseId}';
-			var employeeId = '${employeeId}';
-			var customerId = '${customerId}';
-
-			$('#purchase-id').val(purchaseId);
-			$('#employee-id').val(employeeId);
-			$('#customer-id').val(customerId);
-		});
-		
 		function validateForm() {
 			var employeeId = document.getElementById("employee-id").value;
 			var customerId = document.getElementById("customer-id").value;
@@ -37,27 +24,58 @@ $(document).ready(function() {
 			}
 		}
 
-		function validateSearchForm() {
-		    var purchaseId = document.getElementById("find-purchase-id").value;
+		
+		
+		document.addEventListener("DOMContentLoaded", function() {
+  var searchForm = document.getElementById("search-form");
+  var findPurchaseBtn = document.getElementById("search-btn");
 
-		    if (purchaseId === "" || !/^\d{1,10}$/.test(purchaseId)) {
-		        errorMessage = "Please enter a valid ID (number only) to search for.."
-		        document.getElementById("error-label").innerHTML = errorMessage;
-		        return false;
-		    } else {
-		        var purchaseExists = false;
-		        $('#purchase-table tbody tr').each(function() {
-		            var rowPurchaseId = $(this).find('td:eq(0)').text();
-		            if (rowPurchaseId === purchaseId) {
-		                purchaseExists = true;
-		                return false; // break the loop
-		            }
-		        });
-		        if (!purchaseExists) {
-		            errorMessage = "Purchase with ID " + purchaseId + " doesn't exist.";
-		            document.getElementById("error-label").innerHTML = errorMessage;
-		            return false;
-		        }
-		    }
-		    return true;
-		}
+  findPurchaseBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var strValue = document.getElementById("find-purchase-id").value;
+
+    if (strValue == "" || !/^\d{1,10}$/.test(strValue)) {
+      var errorLabel = document.getElementById("error-label");
+      errorLabel.innerText = "Please enter a valid ID to search for!";
+      return;
+    }
+
+    var tableRows = document.querySelectorAll("#purchase-table tbody tr");
+    var purchaseFound = false;
+
+    tableRows.forEach(function(row) {
+      var purchaseId = row.cells[0].innerText;
+
+      if (purchaseId === strValue) {
+        purchaseFound = true;
+
+        // Retrieve purchase details
+        var customerDetails = row.cells[1].innerText;
+        var employeeDetails = row.cells[2].innerText;
+
+        // Extract customer ID and name
+        var customerId = customerDetails.split("-")[0];
+        var customerName = customerDetails.split("-")[1];
+
+        // Extract employee ID and name
+        var employeeId = employeeDetails.split("-")[0];
+        var employeeName = employeeDetails.split("-")[1];
+
+        // Fill in the text fields
+        document.getElementById("purchase-id").value = strValue;
+        document.getElementById("customer-id").value = customerId;
+        document.getElementById("employee-id").value = employeeId;
+
+      }
+    });
+
+    if (!purchaseFound) {
+      var errorLabel = document.getElementById("error-label");
+      errorLabel.innerText = "Purchase with ID: " + strValue + " not found.";
+    } else {
+      var errorLabel = document.getElementById("error-label");
+      errorLabel.innerText = "Chosen purchase found.";
+    }
+  });
+});
