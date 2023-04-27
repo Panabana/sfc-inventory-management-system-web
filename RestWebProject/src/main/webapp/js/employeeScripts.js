b97a2ea98640bc2c50cd674696ddb19d066bb463
Alex <palexanderbengtsson@gmail.com>
2023-04-27 08:14:10 +0000

    Placed try...catch in every library function

--- RestWebProject/src/main/webapp/js/employeeScripts.js
+++ RestWebProject/src/main/webapp/js/employeeScripts.js
@@ -10,105 +10,131 @@
 
 	// Initialize
 	function initEventListeners() {
-		// Event listener for the select box
-		$('#employeeSelect').on('change', function() {
-			// Retrieve the selected employee ID and name
-			var selectedEmp = $(this).val();
-			console.log(selectedEmp);
-		});
+		try {
+			// Event listener for the select box
+			$('#employeeSelect').on('change', function() {
+				// Retrieve the selected employee ID and name
+				var selectedEmp = $(this).val();
+				console.log(selectedEmp);
+			});
 
-		// Find employee by ID
-		$("#findBtn").click(findEmployeeById());
+			// Find employee by ID
+			$("#findBtn").click(findEmployeeById());
 
-		// Find all employees
-		$("#findAllBtn").click(findAllEmployees());
+			// Find all employees
+			$("#findAllBtn").click(findAllEmployees());
 
-		// Delete employee by ID
-		$("#delEmpBtn").click(deleteEmployeeById);
+			// Delete employee by ID
+			$("#delEmpBtn").click(deleteEmployeeById);
 
-		// Add new employee
-		$("#addEmpBtn").click(addEmployee);
+			// Add new employee
+			$("#addEmpBtn").click(addEmployee);
 
-		// Update employee
-		$("#updtEmpBtn").click(updateEmployee);
+			// Update employee
+			$("#updtEmpBtn").click(updateEmployee);
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
+
 	}
 
 	// Display employee table
 	function displayEmployees(employees) {
-		$.each(employees, function(index, employee) {
-			var row = $("<tr>");
-			row.append($("<td>").text(employee.EmployeeId));
-			row.append($("<td>").text(employee.EmployeeName));
-			row.append($("<td>").text(employee.EmployeeAddress));
-			row.append($("<td>").text(employee.Phone));
-			$("#employeeTable tbody").append(row);
-		});
+		try {
+			$.each(employees, function(index, employee) {
+				var row = $("<tr>");
+				row.append($("<td>").text(employee.EmployeeId));
+				row.append($("<td>").text(employee.EmployeeName));
+				row.append($("<td>").text(employee.EmployeeAddress));
+				row.append($("<td>").text(employee.Phone));
+				$("#employeeTable tbody").append(row);
+			});
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	// Clear employee table
 	function clearTable() {
-		$("#employeeTable tbody").empty();
+		try {
+			$("#employeeTable tbody").empty();
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	// Find employee by ID
 	function findEmployeeById() {
-		$(document).on("click", "#findBtn", function(event) {
-			event.preventDefault();
-			var strValue = $("#empId").val();
-			if (strValue != "") {
-				$.ajax({
-					method: "GET",
-					url: "http://localhost:8080/EJBISWebProject/RestServlet/" + strValue,
-					error: ajaxRestReturnError,
-					success: ajaxRestReturnSuccess
-				})
-			} else {
-				alert("strValue not set");
-			}
-		});
+		try {
+			$(document).on("click", "#findBtn", function(event) {
+				event.preventDefault();
+				var strValue = $("#empId").val();
+				if (strValue != "") {
+					$.ajax({
+						method: "GET",
+						url: "http://localhost:8080/EJBISWebProject/RestServlet/" + strValue,
+						error: ajaxRestReturnError,
+						success: ajaxRestReturnSuccess
+					})
+				} else {
+					alert("strValue not set");
+				}
+			});
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
+
 	}
 
 	// Find all employees
 	function findAllEmployees() {
-		$("#findAllBtn").click(function(event) {
-			event.preventDefault();
-			$.ajax({
-				method: "GET",
-				url: "http://localhost:8080/EJBISWebProject/RestServlet/",
-				success: function(result) {
-					clearTable();
-					displayEmployees(result);
-					$("#error-label-employee").empty();
-					$("#error-label-employee").append("All employees found.");
-				},
-				error: function(xhr, status, error) {
-					console.error("Error in fetching employees:", error);
-					$("#error-label-employee").empty();
-					$(".error-label-employee").append("Error in fetching employees");
-				}
+		try {
+			$("#findAllBtn").click(function(event) {
+				event.preventDefault();
+				$.ajax({
+					method: "GET",
+					url: "http://localhost:8080/EJBISWebProject/RestServlet/",
+					success: function(result) {
+						clearTable();
+						displayEmployees(result);
+						$("#error-label-employee").empty();
+						$("#error-label-employee").append("All employees found.");
+					},
+					error: function(xhr, status, error) {
+						console.error("Error in fetching employees:", error);
+						$("#error-label-employee").empty();
+						$(".error-label-employee").append("Error in fetching employees");
+					}
+				});
 			});
-		});
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	// Delete employee by ID
 	function deleteEmployeeById(event) {
-		$("#delEmpBtn").click(function(event) {
-			event.preventDefault();
-			var strValue = $("#empId").val();
-			if (strValue != "") {
-				$.ajax({
-					method: "DELETE",
-					url: "http://localhost:8080/EJBISWebProject/RestServlet/" + strValue,
-					error: ajaxDelReturnError,
-					success: ajaxDelReturnSuccess
-				})
-			}
-		});
+		try {
+			$("#delEmpBtn").click(function(event) {
+				event.preventDefault();
+				var strValue = $("#empId").val();
+				if (strValue != "") {
+					$.ajax({
+						method: "DELETE",
+						url: "http://localhost:8080/EJBISWebProject/RestServlet/" + strValue,
+						error: ajaxDelReturnError,
+						success: ajaxDelReturnSuccess
+					});
+				}
+			});
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	// Add new employee
 	function addEmployee(event) {
-		
+		try {
 			event.preventDefault();
 			// var strId = $("#empIdAdd").val();
 			var strName = $("#empName").val();
@@ -118,7 +144,7 @@
 			// Removed from obj: EmployeeId: strId, 
 			var obj = { EmployeeName: strName, EmployeeAddress: strAddress, Phone: strPhone };
 			var jsonString = JSON.stringify(obj);
-			
+
 			$.ajax({
 				method: "POST",
 				url: "http://localhost:8080/EJBISWebProject/RestServlet/",
@@ -126,36 +152,43 @@
 				dataType: 'json',
 				error: ajaxAddReturnError,
 				success: ajaxAddReturnSuccess
-			})
-		
+			});
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	// Update employee
 	function updateEmployee() {
-		$("#updtEmpBtn").click(function(event) {
-			event.preventDefault();
-			var strId = $("#empIdAdd").val();
-			var strName = $("#empName").val();
-			var strAddress = $("#empAddress").val();
-			var strPhone = $("#empPhone").val();
-			var obj = { EmployeeId: strId, EmployeeName: strName, EmployeeAddress: strAddress, Phone: strPhone };
-			var jsonString = JSON.stringify(obj);
-			if (strId != "") {
-				$.ajax({
-					method: "PUT",
-					url: "http://localhost:8080/EJBISWebProject/RestServlet/" + strId,
-					data: jsonString,
-					dataType: 'json',
-					error: ajaxUpdateReturnError,
-					success: ajaxUpdateReturnSuccess
-				});
-			}
-		});
+		try {
+			$("#updtEmpBtn").click(function(event) {
+				event.preventDefault();
+				var strId = $("#empIdAdd").val();
+				var strName = $("#empName").val();
+				var strAddress = $("#empAddress").val();
+				var strPhone = $("#empPhone").val();
+				var obj = { EmployeeId: strId, EmployeeName: strName, EmployeeAddress: strAddress, Phone: strPhone };
+				var jsonString = JSON.stringify(obj);
+				if (strId != "") {
+					$.ajax({
+						method: "PUT",
+						url: "http://localhost:8080/EJBISWebProject/RestServlet/" + strId,
+						data: jsonString,
+						dataType: 'json',
+						error: ajaxUpdateReturnError,
+						success: ajaxUpdateReturnSuccess
+					});
+				}
+			});
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	// Populate employee select box
 	function populateEmployeeSelectBox() {
-		$.ajax({
+		try {
+			$.ajax({
 			method: "GET",
 			url: "http://localhost:8080/EJBISWebProject/RestServlet/",
 			success: function(result) {
@@ -170,6 +203,9 @@
 				console.error("Error in fetching employees:", error);
 			}
 		});
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	// Helper functions for AJAX calls
@@ -223,24 +259,33 @@
 	}
 
 	function parseJsonFileEmployee(result) {
-		clearTable();
-		clearFields();
-		var row = $("<tr>");
-		row.append($("<td>").text(result.EmployeeName));
-		row.append($("<td>").text(result.EmployeeId));
-		row.append($("<td>").text(result.EmployeeAddress));
-		row.append($("<td>").text(result.Phone));
-		$("#employeeTable tbody").empty().append(row);
-		$("#empName").val(result.EmployeeName);
-		$("#empIdAdd").val(result.EmployeeId);
-		$("#empAddress").val(result.EmployeeAddress);
-		$("#empPhone").val(result.Phone);
+		try {
+			clearTable();
+			clearFields();
+			var row = $("<tr>");
+			row.append($("<td>").text(result.EmployeeName));
+			row.append($("<td>").text(result.EmployeeId));
+			row.append($("<td>").text(result.EmployeeAddress));
+			row.append($("<td>").text(result.Phone));
+			$("#employeeTable tbody").empty().append(row);
+			$("#empName").val(result.EmployeeName);
+			$("#empIdAdd").val(result.EmployeeId);
+			$("#empAddress").val(result.EmployeeAddress);
+			$("#empPhone").val(result.Phone);
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
 
 	function clearFields() {
-		$("#EmployeeName").text("");
-		$("#EmployeeId").text("");
-		$("#EmployeeAddress").text("");
-		$("#EmployeePhone").text("");
+		try {
+			$("#EmployeeId").text("");
+			$("#EmployeeName").text("");
+			$("#EmployeeAddress").text("");
+			$("#EmployeePhone").text("");
+		} catch (error) {
+			console.error("An error occurred: ", error);
+		}
 	}
+
 })();
