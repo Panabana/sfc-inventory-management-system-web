6c3369282955f21e8451ab47f9431cc914ccbd8f
AxelAlv <ax4821al-s@student.lu.se>
2023-05-02 18:03:27 +0000

    Update purchaseScripts.js

--- EJBISWebProject/src/main/webapp/js/purchaseScripts.js
+++ EJBISWebProject/src/main/webapp/js/purchaseScripts.js
@@ -73,7 +73,6 @@
 		
 		
 		document.addEventListener("DOMContentLoaded", function() {
-  var searchForm = document.getElementById("search-form");
   var findPurchaseBtn = document.getElementById("search-btn");
 
   findPurchaseBtn.addEventListener("click", function(event) {
